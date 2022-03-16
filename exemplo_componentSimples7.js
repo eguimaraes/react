595e9c97cc8bab935f2fbab8b425384bde4e4783
Edvaldo Guimarães <edvaldoguimaraes2013@hotmail.com>
2022-03-15 22:38:05 -0300

    Create exemplo_componentSimples7.js

---
+++ exemplo_componentSimples7.js
@@ -0,0 +1,17 @@
+function ExpenseDate(props){
+
+          const month=props.date.toLocaleString('en-US',{month:'long'});
+          const year=props.date.toLocaleString('en-US',{year:'2-digit'});
+          const day=props.date.getFullYear();
+
+          return (
+<div>
+                              <div>{month}</div>
+                              <div>{year}</div>
+                              <div>{day}</div>
+</div>);
+
+
+}
+
+export default ExpenseDate;
