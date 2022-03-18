55f278f283d5417d288f4499fa080db02de6744e
Edvaldo Guimarães <edvaldoguimaraes2013@hotmail.com>
2022-03-18 14:34:41 -0300

    Create exemplo_componentSimples9.js

---
+++ exemplo_componentSimples9.js
@@ -0,0 +1,31 @@
+import ExpenseItem from "./ExpenseItem";
+
+function ExpensesList(props){
+
+
+          return (
+                    <div>
+                      <ExpenseItem
+                        title={props.items[0].title}
+                        amount={props.items[0].amount}
+                        date={props.items[0].date} 
+                        
+                        />
+                
+                      <ExpenseItem
+                        title={props.items[1].title}
+                        amount={props.items[1].amount}
+                        date={props.items[1].date}
+                        />
+                
+                      <ExpenseItem
+                        title={props.items[2].title}
+                        amount={props.items[2].amount}
+                        date={props.items[2].date}/>
+
+                    </div>
+                  );
+
+}
+
+export default ExpensesList;
