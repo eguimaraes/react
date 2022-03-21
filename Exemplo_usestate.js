5c53f94acf96a265002bebec160a8c78d02719ae
Edvaldo Guimarães <edvaldoguimaraes2013@hotmail.com>
2022-03-21 17:43:28 -0300

    Create Exemplo_usestate.js

---
+++ Exemplo_usestate.js
@@ -0,0 +1,25 @@
+import React,{ useState } from 'react';
+
+import ExpenseDate from './ExpenseDate'
+import Card from './card';
+import './ExpenseItem.css'
+
+
+function ExpenseItem(props) {
+useState();
+          /* const clickHandler=()=>{console.log('Clicked')*/}
+
+          return (
+                    <Card className='expense-item'>
+                              <ExpenseDate date={props.date} />
+                              <div className='expense-item__description'>
+                                        <h2>{props.title}</h2>
+                              </div>
+
+                              <div className='expense-item__price'>${props.amount}</div>
+<button onClick={clickHandler}>Change title</button>
+                    </Card>
+          );
+}
+
+export default ExpenseItem;
